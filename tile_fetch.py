144b66ce4e0d91c52519ae51b8830de1917c138d
Ophir LOJKINE <ophir.lojkine@auto-grid.com>
2019-05-22 17:10:19 +0200

    Implement tile stitching

--- tile_fetch.py
+++ tile_fetch.py
@@ -4,12 +4,12 @@
 import base64
 import hmac
 import re
+from pathlib import Path
 
+from PIL import Image
 import lxml.html
-import argparse
 
 from lxml import etree
-
 import requests
 
 from decryption import decrypt
@@ -56,43 +56,66 @@ def load_image_info(url):
     token_regex = r'"{}","([^"]+)"'.format(part)
     token = re.findall(token_regex, r.text)[0]
 
-    return tile_info, image_name, path, token
+    tile_width = int(meta_info_tree.attrib['tile_width'])
+    tile_height = int(meta_info_tree.attrib['tile_height'])
+
+    return tile_info, (tile_width, tile_height), image_name, path, token
 
 
-def load_tiles(url, z):
-    tile_info, image_name, path, token = load_image_info(url)
+def load_tiles(url, z=6):
+    tile_info, tile_size, image_name, path, token = load_image_info(url)
     try:
         tile = tile_info[z]
     except IndexError:
-        print('Unknown zoom-level %s' % (z, ))
+        print('Unknown zoom-level %s' % (z,))
         quit(1)
 
-    total_tiles = tile['num_tiles_x'] * tile['num_tiles_y']
-    print('Downloading %i tiles' % (total_tiles, ))
-    i = 0
-    for x in range(tile['num_tiles_x']):
-        for y in range(tile['num_tiles_y']):
-            fn = '%s %sx%sx%s.jpg' % (image_name, x, y, z)
-            with open(fn, 'wb') as f:
-                f.write(fetch_tile(path, token, x, y, z))
-            i += 1
-            print('Downloaded %i of %i tiles' % (i, total_tiles), end='\r')
-    print('')
+    tile = tile_info[z]
+    num_tiles_x = int(tile['num_tiles_x'])
+    num_tiles_y = int(tile['num_tiles_y'])
+    empty_x = int(tile['empty_pels_x'])
+    empty_y = int(tile['empty_pels_y'])
+
+    width = num_tiles_x * tile_size[0] - empty_x
+    height = num_tiles_y * tile_size[1] - empty_y
+
+    img = Image.new(mode="RGB", size=(width, height))
+
+    for x in range(num_tiles_x):
+        for y in range(num_tiles_y):
+            percent_complete = 100 * (y + x * num_tiles_y) // (num_tiles_y * num_tiles_x)
+            print("Downloading: {}%".format(percent_complete), end='\r')
+            file_path = Path() / ('%s %sx%sx%s.jpg' % (image_name, x, y, z))
+            if not file_path.exists():
+                tile_bytes = fetch_tile(path, token, x, y, z)
+                file_path.write_bytes(tile_bytes)
+            tile_img = Image.open(file_path)
+            img.paste(tile_img, (x * tile_size[0], y * tile_size[1]))
+    print("Downloaded all tiles")
+    final_image_filename = image_name + '.jpg'
+    img.save(final_image_filename)
+    print("Saved the result as " + final_image_filename)
 
 
 def main():
+    import argparse
+
     parser = argparse.ArgumentParser(description='Download all image tiles from Google Arts and Culture website')
     parser.add_argument('url', type=str, help='an artsandculture.google.com url')
-    parser.add_argument('zoom', type=int, nargs='?', help='Zoom level to fetch, can be negative. Will print zoom levels if omitted')
+    parser.add_argument('--zoom', type=int, nargs='?',
+                        help='Zoom level to fetch, can be negative. Will print zoom levels if omitted')
 
     args = parser.parse_args()
+
     if args.zoom is not None:
         load_tiles(args.url, args.zoom)
     else:
-        tile_info, image_name, path, token = load_image_info(args.url)
+        tile_info, _, image_name, path, token = load_image_info(args.url)
         print('Zoom levels:')
         for i, level in enumerate(tile_info):
-            print(' %i %i x %i (%i tiles)' % (i, level['num_tiles_x'] * 512, level['num_tiles_y'] * 512, (level['num_tiles_x'] * level['num_tiles_y'])))
+            print(' %i %i x %i (%i tiles)' % (
+                i, level['num_tiles_x'] * 512, level['num_tiles_y'] * 512,
+                (level['num_tiles_x'] * level['num_tiles_y'])))
 
 
 if __name__ == '__main__':
